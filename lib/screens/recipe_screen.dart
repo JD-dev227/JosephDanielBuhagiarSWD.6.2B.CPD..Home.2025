834676109777ee0dc118fb578d356e9c8cbb97c0
JD-dev227 <67696522+JD-dev227@users.noreply.github.com>
2025-03-25 19:14:05 +0100

    added recipe screen logic

---
+++ lib/screens/recipe_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ScanScreen extends StatefulWidget {
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  XFile? _image;
+
+  Future<void> _pickImage() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Scan Ingredients')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _image == null
+                ? Text("No image selected")
+                : Image.file(
+                    File(_image!.path),
+                    width: 200,
+                    height: 200,
+                  ),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: Text("Take Picture"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
